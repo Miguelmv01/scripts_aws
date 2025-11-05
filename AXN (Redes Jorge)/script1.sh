3add79e68c96c7961e19667c70e0c521e0997599
Miguel <mikmv2002@gmail.com>
2025-11-05 20:01:27 +0100

    codigo de python y ajustes finales para el .sh

--- AXN (Redes Jorge)/script1.sh
+++ AXN (Redes Jorge)/script1.sh
@@ -28,6 +28,7 @@ aws ec2 modify-subnet-attribute --subnet $SUB_ID --map-public-ip-on-launch
 SG_ID=$(aws ec2 create-security-group --vpc-id $VPC_ID \
  --group-name gsmio \
  --description "Mi grupo de seguridad para abrir el puerto 22" \
+ --query GroupId \
  --output text)
 
  echo "Y ahora el grupo de seguridad"
@@ -35,9 +36,6 @@ SG_ID=$(aws ec2 create-security-group --vpc-id $VPC_ID \
 
 aws ec2 authorize-security-group-ingress \
     --group-id $SG_ID \
-    # --protocol tcp \
-    # --port 22 \
-    # --cidr 0.0.0.0/0 > /dev/null/
     --ip-permissions '[{"IpProtocol": "tcp", "FromPort": 22, "ToPort": 22, "IpRanges": [{"CidrIp": "0.0.0.0/0"}]}]'
 
 aws ec2 create-tags \
@@ -53,10 +51,10 @@ EC2_ID=$(aws ec2 run-instances \
     --security-group-ids $SG_ID \
     --associate-public-ip-address \
     --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=miec2}]' \
-    --query Instances.InstancetId --output text)
+    --query Instances[0].InstanceId --output text)
 
+echo "Esperando 15 segundos a que la instancia se cree..."
 sleep 15
 
-echo $EC2_ID
-
+echo "Instancia EC2 Creada: $EC2_ID"
 
